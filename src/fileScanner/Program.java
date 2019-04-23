4c357c2f08639b909061e5cdbd8111c0b14b8af2
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-04-23 00:23:18 -0300

    Bufferd reader and File reader implemented

--- src/fileScanner/Program.java
+++ src/fileScanner/Program.java
@@ -1,6 +1,8 @@
 package fileScanner;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -9,23 +11,20 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		File file = new File("/users/lucasmancini/in.txt");
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()){
-				System.out.println(sc.nextLine());
+		String path = "/users/lucasmancini/in.txt";
+		
+		
+		try(BufferedReader br = new BufferedReader (new FileReader(path)) ) {
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				
 			}
 		}
 		
 		catch(IOException e) {
 			System.out.println("Error " + e.getMessage());
-		}
-
-		finally {
-			if(sc != null) {
-				sc.close();
-			}
 		}
 	}
 
